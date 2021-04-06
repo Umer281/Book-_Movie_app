28d0199cefdbbcdc4c1d212a591f1ef74dcbce6d
Umer281 <umerbaba86@gmail.com>
2021-04-06 18:23:14 +0100

    added

--- routes/script.js
+++ routes/script.js
@@ -5,16 +5,19 @@ const route=require('express').Router();
 route.post('/',function(req,res){ 
    
     const current_tickets=req.body.tickets_avaliable;
-    if(req.body.tickets_avaliable >0 ){ 
+    // if(req.body.tickets_avaliable >0 ){ 
+        console.log("reached here");
+        console.log(req.body.date);
+
        Tickets.create({ 
-           date:req.body.set_date,
-           time:req.body.set_time,
+           date:req.body.date,
+           time:req.body.time,
            tickets_avaliable:req.body.tickets_avaliable
        }).then((data) => { 
           
         User.create({ 
             movie_name:req.body.movie_name,
-            no_of_tickets:req.body.set_tickets
+            no_of_tickets:req.body.no_of_tickets
         }).then(function(data){ 
             current_tickets=current_tickets-1;
             res.send(current_tickets);
@@ -25,9 +28,9 @@ route.post('/',function(req,res){
            res.send(e);
        })
        
-}else{ 
-    res.send("seats are full");
-}
+// }else{ 
+//     res.send("seats are full");
+// }
 
 
 
